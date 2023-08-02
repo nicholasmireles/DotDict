6d4f20491cc7f070b7a089c35ba7fc10558d341f
Nick Mireles <nick.mireles@dave.com>
2023-08-02 16:38:25 -0400

    migrated to pyproject.tom

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-import os
 from setuptools import setup
 
-HERE = os.path.dirname(os.path.realpath(__file__))
-README = open(os.path.join(HERE, "README.md")).read()
-setup(
-    name="dotdict",
-    version="0.0.1",
-    description="dotdict package.",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    author="Nick Mireles",
-    author_email="nick.mireles@dave.com",
-    license="GPT GPL v3",
-)
+setup()
