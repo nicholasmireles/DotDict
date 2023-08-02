7ca7dd509d63682be86eceea946f7bfdb365e56a
Nick Mireles <nick.mireles@dave.com>
2023-08-02 18:37:11 -0400

    removed unnecessarily overloaded func

--- dotdict.py
+++ dotdict.py
@@ -11,9 +11,6 @@ class DotDict(Namespace):
             return NotImplemented
         return vars(self) == vars(other)
 
-    def __contains__(self, key):
-        return key in self.__dict__
-
     def __getattr__(self, __name: str) -> Any:
         if __name not in self.__dict__:
             self.__dict__[__name] = DotDict()
